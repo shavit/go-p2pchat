dcc28ab405472c5fb0fd78323257ab42bea0345e
shavit <shavit@users.noreply.github.com>
2018-03-19 03:48:53 -0400

    starting the server

--- cmd/run.go
+++ cmd/run.go
@@ -2,13 +2,21 @@ package main
 
 import (
   "os"
+  "../p2p"
 )
 
 func main(){
+  var err error
+  
   if len(os.Args) <= 1 {
     // TODO: Loop through options here
     os.Exit(1)
   }
 
-  println("done")
+  var srv = p2p.NewServer()
+  println(srv)
+  if err = srv.Start(); err != nil {
+    panic(err)
+  }
+
 }
